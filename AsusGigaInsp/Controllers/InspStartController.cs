f8ce315013ef895ac1fc479b6b19df1d6bfb8d78
KikuchiKensuke <kikuchikensuke@adotsu.co.jp>
2020-07-09 09:56:40 +0900

    レビュー用最新版

--- AsusGigaInsp/Controllers/InspStartController.cs
+++ AsusGigaInsp/Controllers/InspStartController.cs
@@ -1,4 +1,7 @@
 ﻿using AsusGigaInsp.Models;
+using AsusGigaInsp.Modules;
+using System.Data.SqlClient;
+using System.Text;
 using System.Web.Mvc;
 
 namespace AsusGigaInsp.Controllers
@@ -18,6 +21,46 @@ namespace AsusGigaInsp.Controllers
         [HttpPost]
         public ActionResult Entry(InspStartModels models)
         {
+            // バリデーションチェック START ***************************************************************
+            DSNLibrary dsnLib = new DSNLibrary();
+            StringBuilder stbSql = new StringBuilder();
+            string[] SerialNOs = models.MasterCartonSerial.Split(',');
+
+            // シリアルの登録がなければエラー
+            StringBuilder SerialErrMsg = new StringBuilder();
+            bool ExistErr = false;
+            SerialErrMsg.Append("シリアル番号：");
+            for (int i = 0; i < SerialNOs.Length; i++)
+            {
+                stbSql.Append("SELECT ");
+                stbSql.Append("   COUNT(1) ");
+                stbSql.Append("FROM dbo.T_SERIAL_STATUS ");
+                stbSql.Append("WHERE SERIAL_NUMBER = '" + SerialNOs[i] + "' ");
+
+                SqlDataReader sqlRdr = dsnLib.ExecSQLRead(stbSql.ToString());
+
+                if (!sqlRdr.HasRows)
+                {
+                    SerialErrMsg.Append("【" + SerialNOs[i] + "】");
+                    ExistErr = true;
+                }
+                stbSql.Clear();
+                sqlRdr.Close();
+            }
+            SerialErrMsg.Append("は登録されていません！");
+
+            dsnLib.DB_Close();
+
+            if (ExistErr)
+                ModelState.AddModelError("MasterCartonSerial", SerialErrMsg.ToString());
+
+            
+            // 既に作業開始されていた場合はエラー
+
+
+
+            // バリデーションチェック END ****************************************************************************
+
             models.UpdateStatus();
             models.SetInspStartSerialLists();
 
